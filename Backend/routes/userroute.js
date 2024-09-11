00948c4d6532a309fd695a3fc39a056179054ddb
CharudattaGhute <ghutecharudatta@gmail.com>
2024-09-12 00:08:35 +0530

    userrouter

--- Backend/routes/userroute.js
+++ Backend/routes/userroute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const usercontroller = require("../Controller/usercontroller");
+const auth = require("../middlware/auth");
 
 const router = express.Router();
 
 router.post("/register", usercontroller.register);
 router.post("/login", usercontroller.login);
+router.get("/userinfo", auth, usercontroller.userinfo);
 
 module.exports = router;
