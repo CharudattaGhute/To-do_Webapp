d8bbc807d37885c2d04a7f617c5c15bd4219df78
CharudattaGhute <ghutecharudatta@gmail.com>
2024-09-11 12:18:58 +0530

    updated

---
+++ Backend/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userrouter = require("./routes/userroute");
+const taskrouter = require("./routes/taskrouter");
+const categoryrouter = require("./routes/categoryrouter");
+const priorityrouter = require("./routes/priorityrouter");
+const inviterouter = require("./routes/inviterouter");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/Todo_task");
+
+const database = mongoose.connection;
+database.on("error", (error) => {
+  console.log("Error", error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+app.use("/api/user", userrouter);
+app.use("/api/task", taskrouter);
+app.use("/api/category", categoryrouter);
+app.use("/api/priority", priorityrouter);
+app.use("/api/invite", inviterouter);
+
+app.listen(5001, () => {
+  console.log("http://localhost:5001");
+});
