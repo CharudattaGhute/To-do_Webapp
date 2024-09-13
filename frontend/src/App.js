dae04d98977921285cb3dd93acddac0438c76b56
CharudattaGhute <ghutecharudatta@gmail.com>
2024-09-13 16:07:51 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Navbar from "./components/Navbar";
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,7 +13,6 @@ import {
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
