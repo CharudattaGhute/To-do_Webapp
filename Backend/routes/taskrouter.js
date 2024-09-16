f41f0b4b9c4cb8bc52651ce6c8268a03ce3bad34
CharudattaGhute <ghutecharudatta@gmail.com>
2024-09-16 19:05:35 +0530

    taskroutre.js

--- Backend/routes/taskrouter.js
+++ Backend/routes/taskrouter.js
@@ -6,7 +6,7 @@ const upload = require("../middlware/multer");
 
 router.post("/addtask", auth, upload, taskcontroller.addtask);
 router.post("/addCollaborator/:id", auth, taskcontroller.addCollaborator);
-router.get("/gettaskbyid/:id", auth, taskcontroller.gettaskbyid);
+router.get("/getTaskById/:taskId", auth, taskcontroller.getTaskById);
 router.get("/getalltask", auth, taskcontroller.getalltask);
 router.put("/updatetask/:id", auth, taskcontroller.updatetask);
 router.delete("/deletetask/:id", auth, taskcontroller.deletetask);
