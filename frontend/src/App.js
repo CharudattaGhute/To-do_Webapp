b6499f78b369480dc502c1044d4a3dd518aa0136
CharudattaGhute <ghutecharudatta@gmail.com>
2024-09-16 19:05:46 +0530

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import {
   BrowserRouter as Router,
